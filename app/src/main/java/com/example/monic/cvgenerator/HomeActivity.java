6dd8ffb2c0cc1ebd1de0f128a87030bcdbe35ccf
monicqx <monica.stanescu95@gmail.com>
2016-11-21 16:52:11 +0200

    hello

--- app/src/main/java/com/example/monic/cvgenerator/HomeActivity.java
+++ app/src/main/java/com/example/monic/cvgenerator/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.monic.cvgenerator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button menuBtn=(Button)findViewById(R.id.H_menuBtn);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/monic/cvgenerator/MenuActivity.java
+++ app/src/main/java/com/example/monic/cvgenerator/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.monic.cvgenerator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        //TODO: create menu
+
+        Button createCV = (Button)findViewById(R.id.M_createCVBtn);
+        createCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CreateCVActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
