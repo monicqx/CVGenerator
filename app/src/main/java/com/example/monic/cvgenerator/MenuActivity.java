b152d4554a6c6eb492a6cc92ec63326d4caa7339
monicqx <monica.stanescu95@gmail.com>
2016-11-21 16:07:00 +0200

    commit

--- app/src/main/java/com/example/monic/cvgenerator/MenuActivity.java
+++ app/src/main/java/com/example/monic/cvgenerator/MenuActivity.java
@@ -9,5 +9,6 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        //TODO: create menu
     }
 }
