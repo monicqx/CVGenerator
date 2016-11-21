3f251c4df8e4d6be37fad8285a210ca0575600ac
monicqx <monica.stanescu95@gmail.com>
2016-11-21 17:19:56 +0200

    hello

--- app/src/main/java/com/example/monic/cvgenerator/CreateCVActivity.java
+++ app/src/main/java/com/example/monic/cvgenerator/CreateCVActivity.java
@@ -9,5 +9,10 @@ public class CreateCVActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_cv);
+
+        //TODO: check if e-mail
+        //TODO: after selecting from spinner: either open a popup window (using fragment), or insert an EditText
+
+
     }
 }
